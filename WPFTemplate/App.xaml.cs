a30d160e00f43be3ff7624b72d90ddf9e8aea74f
dinar <dinar.mobile@gmail.com>
2022-10-05 23:50:23 +0300

    template

---
+++ WPFTemplate/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using CommonWpf.Classes;
+
+namespace WPFTemplate
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App
+	{
+		public App()
+		{
+			AppInitializer.Initialize();
+		}
+	}
+}
---
+++ WPFTemplate/Version.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTemplate
+{
+	public static class Version
+	{
+		public const string NUMBER = "1.0.0.0";
+	}
+}
