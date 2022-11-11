cf370f3e381314bb3a1bcece665e9ff29068e57a
dinar <dinar.mobile@gmail.com>
2022-11-11 05:00:05 +0300

    AudioManager

--- VolumeLockAgent/Program.cs
+++ VolumeLockAgent/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using VideoPlayerController;
 using VolumeLockAgent.Properties;
-using static VolumeLockAgent.WinAPI;
 
 namespace VolumeLockAgent
 {
 	internal static class Program
 	{
+		private static bool needStop;
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
@@ -17,21 +20,58 @@ namespace VolumeLockAgent
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
-			Container components = new Container();
+			using (Container components = new Container())
+			{
+				NotifyIcon notifyIcon = new NotifyIcon(components);
 
-			NotifyIcon notifyIcon = new NotifyIcon(components);
-			notifyIcon.Text = "VolumeLock";
-			notifyIcon.Icon = Resources.icon;
+				notifyIcon.Text = "VolumeLock";
+				notifyIcon.Icon = Resources.icon;
 
-			ContextMenuStrip cmSysTray = new ContextMenuStrip(components);
-			notifyIcon.ContextMenuStrip = cmSysTray;
-			notifyIcon.Visible = true;
+				ContextMenuStrip cmSysTray = new ContextMenuStrip(components);
+				notifyIcon.ContextMenuStrip = cmSysTray;
+				notifyIcon.Visible = true;
 
-			MixerInfo info = WinAPI.GetMixerControls();
+				ToolStripMenuItem closeItem = new ToolStripMenuItem(
+					text: "Close",
+					image: null,
+					onClick: (sender, args) =>
+					         {
+						         needStop = true;
+								 Application.Exit();
+					         });
+				cmSysTray.Items.Add(closeItem);
 
-			VOLUME volume = WinAPI.GetVolume(info);
+				float needVolume = 64f;
 
-			Application.Run();
+				Task.Run(
+					async () =>
+					{
+						try
+						{
+							Thread.CurrentThread.Priority = ThreadPriority.AboveNormal;
+
+							while (!needStop)
+							{
+								float volume = AudioManager.GetMasterVolume();
+
+								if (Math.Abs(needVolume - volume) > float.Epsilon)
+								{
+									AudioManager.SetMasterVolume(needVolume);
+								}
+
+								await Task.Delay(1000);
+							}
+						}
+						finally
+						{
+							Thread.CurrentThread.Priority = ThreadPriority.Normal;
+						}
+					});
+
+				Application.Run();
+
+				needStop = true;
+			}
 		}
 	}
 }
